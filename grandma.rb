cb446aa6003636e46ae326889ec65554e53964d1
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2019-11-18 18:13:46 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,13 @@
 # Write a speak_to_grandma method.
-
+def speak_to_grandma(say_to_grandma)
+  if (say_to_grandma == "I LOVE YOU GRANDMA!")
+    puts "I LOVE YOU TOO PUMPKIN!"
+  elsif (say_to_grandma == say_to_grandma.uppercase)
+    puts "NO, NOT SINCE 1938!"
+  else
+    puts "HUH SPEAK UP, SONNY!"
+  end
+end
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
