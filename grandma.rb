51656fd1b1b24dc9c8883bd198b71f364f28d6ba
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2019-11-18 18:17:58 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -5,7 +5,7 @@ def speak_to_grandma(say_to_grandma)
   elsif (say_to_grandma == say_to_grandma.upcase)
     return "NO, NOT SINCE 1938!"
   else
-    return "HUH SPEAK UP, SONNY!"
+    return "HUH?! SPEAK UP, SONNY!"
   end
 end
 # Whatever you say to grandma, she should respond with
