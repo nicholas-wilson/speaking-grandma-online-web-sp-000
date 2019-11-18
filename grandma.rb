db0106fe359bf3995f4d103e48fe1650ad3edd11
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2019-11-18 18:17:01 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,7 @@
 def speak_to_grandma(say_to_grandma)
   if (say_to_grandma == "I LOVE YOU GRANDMA!")
     return "I LOVE YOU TOO PUMPKIN!"
-  elsif (say_to_grandma == say_to_grandma.uppercase)
+  elsif (say_to_grandma == say_to_grandma.upcase)
     return "NO, NOT SINCE 1938!"
   else
     return "HUH SPEAK UP, SONNY!"
